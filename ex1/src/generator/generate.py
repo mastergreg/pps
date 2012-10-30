fdb5d611649da6b05bd4ad05220a596bb1bb5d5f
Gregory Liras <gregliras@gmail.com>
2012-10-30 18:19:11 +0200

    added generator python :)

---
+++ ex1/src/generator/generate.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+#
+#* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+# File Name : generate.py
+# Creation Date : 30-10-2012
+# Last Modified : Tue 30 Oct 2012 06:18:19 PM EET
+# Created By : Greg Liras <gregliras@gmail.com>
+#_._._._._._._._._._._._._._._._._._._._._.*/
+
+from random import random
+from sys import argv
+
+def help():
+    print """Usage: {0} <matrix size> <output file>
+    """.format(argv[0])
+    exit(1)
+def main():
+    if not len(argv) == 3:
+        help()
+    else:
+        out = open(argv[2], "w")
+        lim = int(argv[1])
+        out.write("{0}\n".format(lim))
+        for i in range(lim):
+            for j in range(lim):
+                out.write("{0}\t".format(100*random()))
+            out.write("\n")
+        out.close()
+
+if __name__=="__main__":
+    main()
+
