35dfd85d3be032a9118f879ea12eb87ead5c4b69
Gregory Liras <gregliras@gmail.com>
2012-11-03 20:37:40 +0200

    added openmp example, cleaned mpi folder

---
+++ ex1/src/openmp/main.c
@@ -0,0 +1,71 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+* File Name : main.c
+* Creation Date : 30-10-2012
+* Last Modified : Thu 01 Nov 2012 04:59:04 PM EET
+* Created By : Greg Liras <gregliras@gmail.com>
+* Created By : Alex Maurogiannis <nalfemp@gmail.com>
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+
+int main(void)
+{
+    int i,j,k;
+    int N;
+    double **A;
+    double l;
+    int tid, nthreads;
+    nthreads = omp_get_num_threads();
+
+    /*
+     * Allocate me!
+     */
+
+    scanf("%d\n", &N);
+
+    A = malloc(N*sizeof(double*));
+    for (k = 0; k < N; k++)
+    {
+        A[k] = malloc(N*sizeof(double));
+    }
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            scanf("%lf", &A[i][j]);
+        }
+    }
+
+
+
+#pragma omp parallel private(tid, i, j, k)
+    {
+        tid = omp_get_thread_num();
+        for (k = tid; k < N - 1; k += nthreads)
+        {
+            for (i = k + 1; i < N; i++)
+            {
+                l = A[i][k] / A[k][k];
+                for (j = k; j < N; j++)
+                {
+                    A[i][j] = A[i][j] -l*A[k][j];
+                }
+            }
+        }
+    }
+
+    //for (i = 0; i < N; i++)
+    //{
+    //    for (j = 0; j < N; j++)
+    //    {
+    //        printf("%lf\t", A[i][j]);
+    //    }
+    //    printf("\n");
+    //}
+
+    return 0;
+}
