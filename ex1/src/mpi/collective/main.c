9402ff312bba08388f79855161eef8e9c843d2a7
Gregory Liras <gregliras@gmail.com>
2012-11-05 10:12:34 +0200

    the calculation is functional but still needs testing

--- ex1/src/mpi/collective/main.c
+++ ex1/src/mpi/collective/main.c
@@ -1,33 +1,43 @@
 /* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
  * File Name : main.c
  * Creation Date : 30-10-2012
- * Last Modified : Mon 05 Nov 2012 01:51:53 AM EET
+ * Last Modified : Mon 05 Nov 2012 10:07:48 AM EET
  * Created By : Greg Liras <gregliras@gmail.com>
  * Created By : Alex Maurogiannis <nalfemp@gmail.com>
  _._._._._._._._._._._._._._._._._._._._._.*/
 
-#include <stdio.h>
 #include <mpi.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <signal.h>
+#include <unistd.h>
 
+#if main_DEBUG
+#define debug(fmt,arg...)     printf("%s: " fmt, __func__ , ##arg)
+#else
+#define debug(fmt,arg...)     do { } while(0)
+#endif
 
 #define NTHREADS 2
 
 static double *allocate_2d(int N, int M)
 {
     double *A;
-    A = malloc(N*M*sizeof(double*));
+    A = malloc(N*M*sizeof(double));
     return A;
 }
 
-static double *parse_matrix_2d(int N, int M, double *A)
+static double *parse_matrix_2d(FILE *fp, int N, int M, double *A)
 {
     int i,j;
     double *p;
     p = A;
     for (i = 0; i < N; i++) {
         for (j = 0; j < M; j++) {
-            scanf("%lf", p++);
+            if(!fscanf(fp, "%lf", p++)) {
+                return NULL;
+            }
         }
     }
     return A;
@@ -59,20 +69,31 @@ static void print_matrix_2d(int N, int M, double *A)
 
 int main(int argc, char **argv)
 {
-    int i,j,k;
+    int j,k;
     int N;
     double l;
     int rank;
-    double *Ak;
-    double *A;
-    double *Ai;
+    double *Ak = NULL;
+    double *A = NULL;
+    double *Ai = NULL;
+
 
     int ret = 0;
+    FILE *fp = NULL;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     if (rank == 0) {
-        scanf("%d\n", &N);
+        debug("rank: %d opens file: %s\n", rank, argv[1]);
+        fp = fopen(argv[1], "r");
+        if(fp) {
+            if(!fscanf(fp, "%d\n", &N)) {
+                MPI_Abort(MPI_COMM_WORLD, 1);
+            }
+        }
+        else {
+            raise(SIGKILL);
+        }
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
@@ -86,7 +107,11 @@ int main(int argc, char **argv)
     if (rank == 0) {
         /* Root Allocates the whole table */
         A = allocate_2d(N, N);
-        parse_matrix_2d(N, N, A);
+        if(!parse_matrix_2d(fp, N, N, A)) {
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        fclose(fp);
+        fp = NULL;
     } 
 
 
@@ -94,18 +119,21 @@ int main(int argc, char **argv)
     for (k = 0; k < N - 1; k++) {
         if (rank == 0) {
             Ak = &A[k*N];
+            debug("k %d\n", k);
         }
         /* Send everyone the k-th row, and scatter the rest of the table */
         MPI_Barrier(MPI_COMM_WORLD);
-        printf("k %d\n", k);
+
         MPI_Bcast(Ak, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
         MPI_Barrier(MPI_COMM_WORLD);
         MPI_Scatter(&A[N * (k + 1)], N, MPI_DOUBLE, Ai, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
         MPI_Barrier(MPI_COMM_WORLD);
 
+#if main_DEBUG
         if(rank == 0) {
             print_matrix_2d(N, N, A);
         }
+#endif
 
         // every run has the same k but they have different ranks
         l = Ai[k + rank] / Ak[k + rank];
@@ -117,26 +145,26 @@ int main(int argc, char **argv)
         MPI_Gather(Ai, N , MPI_DOUBLE, &(A[N * (k + 1)]), N, MPI_DOUBLE, 0 ,MPI_COMM_WORLD);
         MPI_Barrier(MPI_COMM_WORLD);
     }
+#if MAIN_DEBUG
     if(rank == 0) {
         print_matrix_2d(N, N, A);
     }
+#endif
 
-    
     MPI_Barrier(MPI_COMM_WORLD);
 
-
-    if(!MPI_Finalized(&ret)) {
-        printf("Now FINALIZE!!!\n");
-        ret = MPI_Finalize();
-        printf("%d FINALIZED!!!\n", ret);
+    ret = MPI_Finalize();
+    if(ret == 0) {
+        debug("%d FINALIZED!!! with code: %d\n", rank, ret);
     }
-    MPI_Barrier(MPI_COMM_WORLD);
+    else {
+        debug("%d NOT FINALIZED!!! with code: %d\n", rank, ret);
+    }
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            printf("%lf\t", A[i*N+j]);
-        }
-        printf("\n");
+
+    if(rank == 0) {
+        sleep(1);
+        print_matrix_2d(N, N, A);
     }
 
     return 0;
--- ex1/src/mpi/collective/run.sh
+++ ex1/src/mpi/collective/run.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-make
-mpirun -np 5 ./main.exec < mat.txt
+
+
+NTHREADS=13
+
+
+if [[ 'x'$1 == 'x' ]]; 
+then
+    echo "No arguements, simple run, for debug ./run.sh d"
+    make -B
+    mpirun -np ${NTHREADS} ./main.exec mat.txt
+else
+    if [[ 'x'$1 == 'xd' ]];
+    then
+        make -B DEBUG=y
+        echo "Running with valgrind"
+        MPIWRAP_DEBUG=warn \
+            LD_PRELOAD=/usr/lib/valgrind/libmpiwrap-x86-linux.so \
+            mpirun -np ${NTHREADS} valgrind --leak-check=full --log-file=val.out ./main.exec mat.txt
+    else
+        echo "Bad arguements, no run, for debug ./run.sh d"
+    fi
+fi
