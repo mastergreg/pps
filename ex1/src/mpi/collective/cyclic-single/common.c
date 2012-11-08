631ffc7a4386dc90008a5e51b7871650c2482d3a
Alex Mavrogiannis <nalfemp@gmail.com>
2012-11-08 16:59:33 +0200

    draft of single-broadcast cyclic implementation @mastergreg

---
+++ ex1/src/mpi/collective/cyclic-single/common.c
@@ -0,0 +1,89 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+* File Name : common.c
+* Creation Date : 06-11-2012
+* Last Modified : Wed 07 Nov 2012 08:27:54 PM EET
+* Created By : Greg Liras <gregliras@gmail.com>
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include "common.h"
+#include <sys/time.h>
+
+double *allocate_2d(int N, int M)
+{
+    double *A;
+    A = malloc(N * M * sizeof(double));
+    return A;
+}
+
+double *allocate_2d_with_padding(int N, int M, int max_rank)
+{
+    double *A;
+    A = allocate_2d(N + max_rank, M);
+    return A;
+}
+
+
+double *parse_matrix_2d(FILE *fp, int N, int M, double *A)
+{
+    int i,j;
+    double *p;
+    p = A;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            if(!fscanf(fp, "%lf", p++)) {
+                return NULL;
+            }
+        }
+    }
+    return A;
+}
+
+void fprint_matrix_2d(FILE *fp, int N, int M, double *A)
+{
+    int i,j;
+    double *p;
+    p = A;
+    for (j = 0; j < M; j++) {
+        fprintf(fp, "=");
+    }
+    fprintf(fp, "\n");
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            fprintf(fp, "%lf\t", *p++);
+        }
+        fprintf(fp, "\n");
+    }
+    for (j = 0; j < M; j++) {
+        fprintf(fp, "=");
+    }
+    fprintf(fp, "\n");
+}
+
+void print_matrix_2d(int N, int M, double *A)
+{
+    fprint_matrix_2d(stdout, N, M, A);
+}
+
+
+double timer(void)
+{
+    static double seconds = 0;
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    if(seconds == 0) {
+        seconds = tv.tv_sec + (((double) tv.tv_usec)/1e6);
+        return 0;
+    }
+    else {
+        return tv.tv_sec + (((double) tv.tv_usec)/1e6) - seconds;
+    }
+}
+
+void usage(int argc, char **argv)
+{
+    if(argc != 3) {
+        printf("Usage: %s <matrix file> <output file>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+}
+
