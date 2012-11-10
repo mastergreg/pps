0a9080b626903641a1d668c89e92fa4434245736
Alex Mavrogiannis <nalfemp@gmail.com>
2012-11-10 04:07:02 +0200

    added hybrid version as a generalization of the previous one @mastergreg

---
+++ ex1/src/mpi/collective/hybrid/main.c
@@ -0,0 +1,133 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+ * File Name : main.c
+ * Creation Date : 30-10-2012
+ * Last Modified : Thu 08 Nov 2012 09:49:47 AM EET
+ * Created By : Greg Liras <gregliras@gmail.com>
+ * Created By : Alex Maurogiannis <nalfemp@gmail.com>
+ _._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "common.h"
+
+#define BLOCK_ROWS 2
+
+
+/*      process_rows: performs the calculations for a given set of rows.
+ *          In this hybrid version each thread is assigned blocks of 
+ *          continuous rows in a cyclic manner.
+ */
+void process_rows(int k, int rank, int N, int max_rank, double *A){
+    int i, j, w;
+    double l;
+    /* For every cyclic repetition of a block */
+    for (i = (rank + ((BLOCK_ROWS * max_rank) * (k / (BLOCK_ROWS * max_rank)))); i < N ; i+=(max_rank * BLOCK_ROWS)) {
+            if (i > k) {
+                /* Calculate each continuous row in the block*/
+                for (w = i; w < (i + BLOCK_ROWS) && w < (N * N); w++){
+                    l = A[(w * N) + k] / A[(k * N) + k];
+                    for (j = k; j < N; j++) {
+                        A[(w * N) + j] = A[(w * N) + j] - l * A[(k* N) + j];
+                    }
+                }
+            }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int k;
+    int N;
+    int rank;
+    int max_rank;
+    int last_rank;
+    double *A = NULL;
+    double sec = 0;
+
+    int ret = 0;
+    FILE *fp = NULL;
+    usage(argc, argv);
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &max_rank);
+
+    if (rank == 0) {
+        debug("rank: %d opens file: %s\n", rank, argv[1]);
+        fp = fopen(argv[1], "r");
+        if(fp) {
+            if(!fscanf(fp, "%d\n", &N)) {
+                MPI_Abort(MPI_COMM_WORLD, 1);
+            }
+        }
+        else {
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    /* Everyone allocates the whole table */
+    debug("Max rank = %d\n", max_rank);
+    if((A = allocate_2d_with_padding(N, N, max_rank)) == NULL) {
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+    /* Root Parses file */
+    if (rank == 0) {
+        if(parse_matrix_2d(fp, N, N, A) == NULL) {
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        fclose(fp);
+        fp = NULL;
+    }
+    /* And distributes the table */
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Bcast(A, N*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    last_rank = (N - 1) % max_rank;
+ 
+    if(rank == 0) {
+        sec = timer();
+    }
+
+    for (k = 0; k < N - 1; k++) {
+        /* The owner of the row for this k broadcasts it*/
+        MPI_Barrier(MPI_COMM_WORLD);
+        MPI_Bcast(&A[k * N], N, MPI_DOUBLE, ((k % (max_rank * BLOCK_ROWS)) / BLOCK_ROWS), MPI_COMM_WORLD);
+
+        process_rows(k, rank, N, max_rank, A);
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (rank == 0) {
+        sec = timer();
+        printf("Calc Time: %lf\n", sec);
+    }
+    ret = MPI_Finalize();
+
+    if(ret == 0) {
+        debug("%d FINALIZED!!! with code: %d\n", rank, ret);
+    }
+    else {
+        debug("%d NOT FINALIZED!!! with code: %d\n", rank, ret);
+    }
+
+    /* Last process has table */
+    if (rank == last_rank) {
+        //print_matrix_2d(N, N, A);
+        fp = fopen(argv[2], "w");
+        fprint_matrix_2d(fp, N, N, A);
+        fclose(fp);
+    }
+    free(A);
+
+    return 0;
+}
--- ex1/src/testsuite/testall.sh
+++ ex1/src/testsuite/testall.sh
@@ -9,7 +9,7 @@ serialpath=../serial/main.exec
 testfilesSizes=(5 15 42 100 1000)
 #testfiles=(mat_5.txt mat_15.txt mat_42.txt mat_100.txt mat_1000.txt mat_10000.txt)
 testfiles=(mat_5.txt mat_15.txt mat_42.txt mat_100.txt mat_1000.txt)
-testfolders=(../mpi/collective/cyclic-single/ ../mpi/collective/continuous)
+testfolders=(../mpi/collective/hybrid/ ../mpi/collective/cyclic-single)
 NTHREADS=2
 
 nr=${#testfiles[@]}
