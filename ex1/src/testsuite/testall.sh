dd3b0d6ca8fcdd216b50a584bf1c504155109ea6
Alex Mavrogiannis <nalfemp@gmail.com>
2012-11-11 21:16:07 +0200

    added continuous propagated version

--- ex1/src/testsuite/testall.sh
+++ ex1/src/testsuite/testall.sh
@@ -10,8 +10,7 @@ testfilesSizes=(5 15 42 100 1000)
 #testfiles=(mat_5.txt mat_15.txt mat_42.txt mat_100.txt mat_1000.txt mat_10000.txt)
 testfiles=(mat_5.txt mat_15.txt mat_42.txt mat_100.txt mat_1000.txt)
 #testfolders=(../mpi/collective/hybrid/ ../mpi/collective/cyclic-single ../mpi/ptp/hybrid/)
-testfolders=( ../mpi/ptp/hybrid)
-testfolders=( ../mpi/collective/continuous-single)
+testfolders=( ../mpi/ptp/continuous-single)
 NTHREADS=2
 
 nr=${#testfiles[@]}
