1d071a0ec45d120b01501284ac5017c60889457c
Gregory Liras <gregliras@gmail.com>
2012-11-13 10:26:50 +0200

    updated gitignore and testall to benchmarh everything

--- ex1/src/testsuite/testall.sh
+++ ex1/src/testsuite/testall.sh
@@ -7,7 +7,6 @@ diffpath=../diffpy/diff.py
 serialpath=../serial/main.exec
 #testfilesSizes=(5 15 42 100 1000)
 testfilesSizes=(100 1000 2000)
-#testfiles=(mat_5.txt mat_15.txt mat_42.txt mat_100.txt mat_1000.txt mat_10000.txt)
 MPItestfolders=(../mpi/ptp/continuous-single ../mpi/ptp/hybrid ../mpi/collective/hybrid ../mpi/collective/continuous-single)
 OPENMPtestfolders=( ../openmp)
 NTHREADS=2
@@ -16,15 +15,15 @@ nr=${#testfiles[@]}
 
 for i in ${testfilesSizes[@]}
 do
-    testfiles[${i}]="mat_${i}.txt"
+    testfiles[${i}]="mat_${i}.in"
 done
 
 if [ 'x'$1 == 'xgen' ];
 then
     for i in ${testfilesSizes[@]}
     do
-        echo "Generating mat_${i}.txt"
-        ${genpathpath} ${i} "mat_${i}.txt"
+        echo "Generating mat_${i}.in"
+        ${genpathpath} ${i} "mat_${i}.in"
     done
 else
     echo "Not generating files (takes too long every time right?)"
@@ -34,7 +33,7 @@ fi
 for i in ${testfiles[@]}
 do
     out="${j//\.\.\//}"
-    serialfile="serial_${i%txt}out"
+    serialfile="serial_${i%in}out"
     ${serialpath} ${i} ${serialfile}
 done
 
@@ -46,8 +45,8 @@ do
     do
         echo $i
         out="${j//\.\.\//}"
-        outfile="${out//\//_}_${i%txt}out"
-        serialfile="serial_${i%txt}out"
+        outfile="${out//\//_}_${i%in}out"
+        serialfile="serial_${i%in}out"
         mpirun -np ${NTHREADS} ${j}/main.exec ${i} ${outfile}
         ${diffpath} ${serialfile} ${outfile}
     done
@@ -61,8 +60,8 @@ do
     do
         echo $i
         out="${j//\.\.\//}"
-        outfile="${out//\//_}_${i%txt}out"
-        serialfile="serial_${i%txt}out"
+        outfile="${out//\//_}_${i%in}out"
+        serialfile="serial_${i%in}out"
         ${j}/main.exec ${i} ${outfile}
         ${diffpath} ${serialfile} ${outfile}
     done
