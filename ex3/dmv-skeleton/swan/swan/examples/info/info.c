cf99373741dd05cd016b5f405612a5b27f637aaf
Gregory Lyras <gregliras@gmail.com>
2013-02-28 17:56:29 +0200

    removed false folder @afein

--- ex3/dmv-skeleton/swan/swan/examples/info/info.c
+++
@@ -1,8 +0,0 @@
-#include "swan_api.h"
-
-int main(void) {
-	swanEnumerateDevices( stdout );
-
-	printf("THIS DEVICE HAS %d CEs\n", swanGetNumberOfComputeElements() );
-}
-
--- ex3/dmv-skeleton/swan/swan/src/checkpoint.c
+++
@@ -1,7 +0,0 @@
-void swanEnableCheckpointing( void ) {
-  if( state.init ) {
-    error( "Cannot enable checkpointing once Swan is initialised" );
-  }
-  state.checkpointing = 1;
-}
-
